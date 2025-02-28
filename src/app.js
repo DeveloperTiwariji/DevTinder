ab3f3f4607d356a21297a9cf7231d0f84782fded
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2025-02-28 21:57:22 +0530

    Play with multiple route handaler

--- src/app.js
+++ src/app.js
@@ -3,21 +3,20 @@ const express = require("express");
 const app = express();
 
 
-app.get("/user/:userId", (req,res) =>{
-    console.log(req.params);
-    res.send({name: "Satyam", age: 21});
-})
-
-app.post("/user", (req,res) =>{
-    console.log(req.query);
-    res.send("Data is add to database successfully");
-})
-
-
-app.delete("/user", (req,res) =>{
-    res.send("Data is deleted from database successfully");
+app.use("/user", (req,res, next)=>{
+    console.log("Handling the route user!!");
+    next();
+}, (req,res,next)=>{
+    console.log("Handling the route user 2!!!");
+    // res.send("User page");
+    next();
+},(req,res, next)=>{
+    console.log("Handling the route user3!!");
+    res.send("User page");
+    // next();
 })
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000");
+    
 })
