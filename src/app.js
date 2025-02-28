e203ab217c73a8b6f6b6ece9c858f8d667ae5987
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2025-02-28 20:37:44 +0530

    Playing wiht the routes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req,res) =>{
-    res.send("Hello Dahsboard 2");
-});
+
+app.get("/user/:userId", (req,res) =>{
+    console.log(req.params);
+    res.send({name: "Satyam", age: 21});
+})
+
+app.post("/user", (req,res) =>{
+    console.log(req.query);
+    res.send("Data is add to database successfully");
+})
+
+
+app.delete("/user", (req,res) =>{
+    res.send("Data is deleted from database successfully");
+})
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000");
