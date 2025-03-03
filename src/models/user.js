96d842b4061de7f44b748c5c44de242bad66f047
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2025-03-03 20:09:43 +0530

    schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,54 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
+        minlength:4,
+        maxlength:20,
     },
     lastName:{
         type:String,
     },
     email:{
         type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+
     },
     password:{
-        tyep:String,
+        type:String,
+        required:true,
+        minlength:8,
+        maxlength:128,
+        
     },
     age:{
         type:Number,
+        min:18,
+        max:100,
     },
     gender:{
         type:String,
+        validate(value){
+            if(!["male","female","other"].includes(value)){
+                throw new Error("Gender Data is not valid");
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://www.google.com",
+
+    },
+    about:{
+        type:String,
+        maxlength:500,
+    },
+    skills:{
+        type:[String],
     }
-});
+}, {timestamps:true});
 
 
 
